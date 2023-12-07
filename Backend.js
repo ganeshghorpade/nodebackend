3568f8eb7e573aa4bbe90f458af026d88bb77e6a
ganesh <ganeshghorpade434@gmail.com>
2023-12-08 01:38:21 +0530

    first commit

---
+++ Backend.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/about',(req,res) => {
+    res.send('About.com')
+})
+app.get('/login',(req,res) => {
+    res.send('<h1>Login Done</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
